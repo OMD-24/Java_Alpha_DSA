03da3d5a92fac58dceaaf1c12779f47f88a590cc
om <omdeshmukh240@gmail.com>
2025-03-16 16:41:13 +0530

    Nth number in fabonacci series

---
+++ src/Recursion/FibonacciNumber.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+    public static int fibo(int n){
+        if(n==1){
+            return 0;
+        }
+        else if(n==2){
+            return 1;
+        }
+        int fibon = fibo(n-1) + fibo(n-2);
+        return fibon;
+    }
+
+    public static void main(String [] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        n=sc.nextInt();
+        System.out.println(fibo(n));
+
+    }
+}
